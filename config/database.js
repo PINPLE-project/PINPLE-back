6f0dbd0206b077ce4518fffc6260c5aaf526a7ed
moonyaeyoon <moonyaeyoon@naver.com>
2023-07-14 14:45:14 +0900

    july 14

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 // 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '',
-    password: '',
+    host: 'localhost',
+    user: 'moonyaeyoon',
+    port: '3000',
+    password: 'yaeyoon1004!',
     database: ''
 });
 
