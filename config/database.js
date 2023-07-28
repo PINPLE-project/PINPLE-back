00acbcb6b9560f8ebdbfb1dfd98c067b064d1370
jyjyjy25 <jyjyjy7418@gmail.com>
2023-07-28 18:20:54 +0900

    [ADD] database 정보 추가

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 // 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '',
-    password: '',
-    database: ''
+  host: "127.0.0.1",
+  user: "root",
+  port: "3306",
+  password: "jyjyjy1106@",
+  database: "pinple",
 });
 
 module.exports = {
-    pool: pool
+  pool: pool,
 };
