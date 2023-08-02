d5a8c67e9a06cd4114db7a92d7862a973a364d18
Seohyeon <drif0023@gmail.com>
2023-08-02 17:27:25 +0900

    [DEL] pinViewProvider: deleteOldMyPin 삭제

--- src/app/PinView/pinViewProvider.js
+++ src/app/PinView/pinViewProvider.js
@@ -16,7 +16,6 @@ exports.retrievePin = async function (pinId, userId) {
     const connection = await pool.getConnection(async (conn) => conn);
     const recentPinIdResult1 = await pinViewDao.deleteRecentPin(connection, pinId, userId);
     const recentPinIdResult2 = await pinViewDao.insertRecentPin(connection, pinId, userId);
-    await pinViewDao.deleteOldMyPin(connection);
     const pinByIdResult = await pinViewDao.selectPinById(connection, pinId, userId);
     connection.release();
   
