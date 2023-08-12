f0e1363edcfb5ec63459fa35b8d00eb24f93485d
jyjyjy25 <jyjyjy7418@gmail.com>
2023-08-12 21:54:08 +0900

    [FEAT] 핀 알림 구현

--- src/app/PinView/pinViewService.js
+++ src/app/PinView/pinViewService.js
@@ -1,90 +1,121 @@
 const { pool } = require("../../../config/database");
+const { logger } = require("../../../config/winston");
 const pinViewDao = require("./pinViewDao");
 const pinViewProvider = require("./pinViewProvider");
 const baseResponse = require("../../../config/responseStatus");
-const {response, errResponse} = require("../../../config/response");
+const { response, errResponse } = require("../../../config/response");
 
 //핀지도 생성
-exports.createPin = async function (userId, longitude, latitude, address, pinCongest, pinFeeling, contents) {
-    try {
+exports.createPin = async function (
+  userId,
+  longitude,
+  latitude,
+  address,
+  pinCongest,
+  pinFeeling,
+  contents
+) {
+  try {
+    const insertPinParams = [
+      userId,
+      longitude,
+      latitude,
+      address,
+      pinCongest,
+      pinFeeling,
+      contents,
+    ];
 
-        const insertPinParams = [userId, longitude, latitude, address, pinCongest, pinFeeling, contents];
+    const connection = await pool.getConnection(async (conn) => conn);
 
-        const connection = await pool.getConnection(async (conn) => conn);
-
-        const pinIdResult = await pinViewDao.insertPin(connection, insertPinParams);
-        const nearByPinIdResult = await pinViewDao.insertNearByPin(connection, pinId, pinCongest);
-        // console.log(`추가된 핀지도 : ${pinIdResult[0]}`)
-        connection.release();
-        return response(baseResponse.SUCCESS);
-
-
-    } catch (err) {
-        logger.error(`App - createPin Service error\n: ${err.message}`);
-        console.log("createPin error");
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    const pinIdResult = await pinViewDao.insertPin(connection, insertPinParams);
+    const nearByPinIdResult = await pinViewDao.insertNearByPin(
+      connection,
+      pinId,
+      pinCongest
+    );
+    // console.log(`추가된 핀지도 : ${pinIdResult[0]}`)
+    connection.release();
+    return response(baseResponse.SUCCESS);
+  } catch (err) {
+    logger.error(`App - createPin Service error\n: ${err.message}`);
+    console.log("createPin error");
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 //핀지도 삭제
-exports.deletePin = async function(pinId){
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const pinIdResult = await pinViewDao.deletePin(connection, pinId);
-        // console.log(`삭제된 핀지도 : ${pinIdResult[0]}`)
-        connection.release();
-        return response(baseResponse.SUCCESS);
-
-    } catch (err) {
-        logger.error(`App - deletePin Service error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+exports.deletePin = async function (pinId) {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const pinIdResult = await pinViewDao.deletePin(connection, pinId);
+    // console.log(`삭제된 핀지도 : ${pinIdResult[0]}`)
+    connection.release();
+    return response(baseResponse.SUCCESS);
+  } catch (err) {
+    logger.error(`App - deletePin Service error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 //도움이 되었어요
-exports.postPinLike = async function(pinId, userId){
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const likeCountResult = await pinViewDao.updatePinLikeCount1(connection, pinId);
-        const likeUserResult = await pinViewDao.insertPinLikeUser(connection, pinId, userId);
-        
-        connection.release();
-        return response(baseResponse.SUCCESS);
+exports.postPinLike = async function (pinId, userId) {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const likeCountResult = await pinViewDao.updatePinLikeCount1(
+      connection,
+      pinId
+    );
+    const likeUserResult = await pinViewDao.insertPinLikeUser(
+      connection,
+      pinId,
+      userId
+    );
 
-    } catch (err) {
-        logger.error(`App - postPinLike Service error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    connection.release();
+    return response(baseResponse.SUCCESS);
+  } catch (err) {
+    logger.error(`App - postPinLike Service error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
 //도움이 되었어요 취소
-exports.deletePinLike = async function(pinId, userId){
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const likeCountResult = await pinViewDao.updatePinLikeCount2(connection, pinId);
-        const likeUserResult = await pinViewDao.deletePinLikeUser(connection, pinId, userId);
+exports.deletePinLike = async function (pinId, userId) {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const likeCountResult = await pinViewDao.updatePinLikeCount2(
+      connection,
+      pinId
+    );
+    const likeUserResult = await pinViewDao.deletePinLikeUser(
+      connection,
+      pinId,
+      userId
+    );
 
-        connection.release();
-        return response(baseResponse.SUCCESS);
-
-    } catch (err) {
-        logger.error(`App - deletePinLike Service error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+    connection.release();
+    return response(baseResponse.SUCCESS);
+  } catch (err) {
+    logger.error(`App - deletePinLike Service error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
 
-
 //최근 조회한 핀지도 삭제
-exports.deleteMyPin = async function(userId, pinId){
-    try {
-        const connection = await pool.getConnection(async (conn) => conn);
-        const myPinIdResult = await pinViewDao.deleteMyPin(connection, userId, pinId);
-        // console.log(`삭제된 핀지도 : ${pinIdResult[0]}`)
-        connection.release();
-        return response(baseResponse.SUCCESS);
-
-    } catch (err) {
-        logger.error(`App - deleteMyPin Service error\n: ${err.message}`);
-        return errResponse(baseResponse.DB_ERROR);
-    }
+exports.deleteMyPin = async function (userId, pinId) {
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const myPinIdResult = await pinViewDao.deleteMyPin(
+      connection,
+      userId,
+      pinId
+    );
+    // console.log(`삭제된 핀지도 : ${pinIdResult[0]}`)
+    connection.release();
+    return response(baseResponse.SUCCESS);
+  } catch (err) {
+    logger.error(`App - deleteMyPin Service error\n: ${err.message}`);
+    return errResponse(baseResponse.DB_ERROR);
+  }
 };
